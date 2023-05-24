6af12ed39b797433268b681b523a8159e574586a
DESKTOP-F5DPU91 <iamdipakbudha@gmail.com>
2023-05-24 10:15:10 +0545

    register form

--- index.php
+++ index.php
@@ -1,79 +1,46 @@
-octype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-
-    <title>Hello, world!</title>
-  </head>
-  <body>
-    <h1>Hello, world!</h1>
-    
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
+<?php include 'header.php';?>
 
 <div class="container">
 
-<!DOCTYPE HTML>
-<html>
-    <body>
-        <form action = "Welcome" method = "post">
-            Name : <input type = "text" name = "name"> <br>
 
-            E-mail : <input type = "text" name = "email"> <br>
+    
+<div class ="col-sm-12">
 
-            <label for = "Uname">username </label>
-            <input type = "text" placeholder = "Enter Username" name = "Uname" required><br>
+<div class="col-sm-8 offset-2">
 
+<legend style="margin-left: 60px; color:purple; margin-top:20px;">Signup Form </legend>
+<form>
 
-            <label for = "Psw"> Password </label>
-            <input type = "Password" placeholder = " Enter password" name = "psw" required><br>
+<div class="col-sm-6">
+<div class ="form-group">
+<labe>Name</label>
+ <input type="text" name="name" class="form-control">
+</div>
+</div>
 
+<div class="col-sm-6">
+<div class ="form-group">
+<labe>Email</label>
+ <input type="email" name="email" class="form-control">
+</div>
+</div>
 
-            <button type = "submit">login </button><br>
+<div class="col-sm-6">
+<div class ="form-group">
+<labe>Password</label>
+ <input type="password" name="password" class="form-control">
+</div>
 
-            
-    <span class="psw">Forgot <a href="#">password</a></span>
+</div>
 
-    </body>
-</html>
+<div class="col-sm-6">
+<button class="btn btn-primary"  style="margin-left: 9~0px;"> Signup</button>
+</div>
+
+</form>
+</div>
+</div>
 </div>
     
   </body>
